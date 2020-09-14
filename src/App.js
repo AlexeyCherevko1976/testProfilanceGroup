010ec3955a1ac4fa93ed33a5133410677f463a07
Alexey Cherevko <you@example.com>
2020-09-14 15:21:29 +0700

    15-20 navbar vrode nichego

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ import './bootstrap.css';
 function App() {
   return (
 
-<div className="container App">
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-<button type="button" className="btn btn-light">Главная</button>
-<button type="button" className="btn btn-light">Новости</button>
-<button type="button" className="btn btn-light">Вход/Выход</button>
-
-
-</nav>
+<div className="container-fluid App">
   <div className="row">
-    <div className="col-sm-8 col-md-6 col-4">
+    <div className="col-12">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
+        <button type="button" className="btn btn-outline-primary btn-block">Главная</button>
+        <button type="button" className="btn btn-outline-secondary btn-block">Новости</button>
+        <button type="button" className="btn btn-outline-success btn-block">Вход/Выход</button>
+
+      </nav>
     </div>
   </div>
-
-
+  <div className="row">
+    <div className="col-12">
+      <div className="App-content">
+        Содержание
+      </div>  
+    </div>  
+  </div>
 </div>
+ 
   );
 }
 
