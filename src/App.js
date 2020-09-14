19072f4b805ef4b4bdc4ef23f6efd17d88e52545
Alexey Cherevko <you@example.com>
2020-09-14 15:52:38 +0700

    function App v klass

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import './bootstrap.css';
 
-function App() {
+class App extends React.Component {
+
+constructor(props){
+  super(props);
+}
+componentDidMount(){
+
+}
+
+render(){
   return (
 
-<div className="container-fluid App">
-  <div className="row">
-    <div className="col-12">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-        <button type="button" className="btn btn-outline-primary btn-block">Главная</button>
+<div className="container-fluid App">
+  <div className="row App-nav">
+    <div className="col-12">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+
+        <button type="button" className="btn btn-outline-primary btn-block ">Главная</button>
         <button type="button" className="btn btn-outline-secondary btn-block">Новости</button>
         <button type="button" className="btn btn-outline-success btn-block">Вход/Выход</button>
-
+ 
       </nav>
     </div>
+
   </div>
   <div className="row">
     <div className="col-12">
-      <div className="App-content">
+      <div className="App-content"> 
         Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        Содержание
+        
       </div>  
     </div>  
   </div>
 </div>
  
-  );
-}
+  ); 
+
+} // render
+} // App
 
 export default App;
