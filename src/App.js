40e04848b8f05a9fcbd23881d6bef01f760361fb
Alexey Cherevko <you@example.com>
2020-09-14 14:38:23 +0700

    14-37 add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="container App">
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<button type="button" className="btn btn-light">Главная</button>
+<button type="button" className="btn btn-light">Новости</button>
+<button type="button" className="btn btn-light">Вход/Выход</button>
+
+
+</nav>
+  <div className="row">
+    <div className="col-sm-8 col-md-6 col-4">
+
     </div>
+  </div>
+
+
+</div>
   );
 }
 
